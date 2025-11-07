14416d7b8b55cb90fb6dd51d2ddd5a58ab451391
10555gff <1073245193@qq.com>
2025-11-07 03:47:20 -0700

    kk

---
+++ src/common/bluetooth.ts
@@ -0,0 +1,25 @@
+
+export async function deviceConnect(): Promise<void> {
+  console.log("请求 BLE 设备");
+  if (!navigator.bluetooth) {
+    console.error("当前浏览器不支持 Web Bluetooth API");
+    return;
+  }
+
+  try {
+    const device = await navigator.bluetooth.requestDevice({
+      acceptAllDevices: true,
+      //optionalServices: ["battery_service"], // 可以换成你需要的 Service UUID
+    });
+
+    console.log("设备:", device.name || device.id);
+
+    // 如果需要可以进一步连接 GATT
+    // const server = await device.gatt?.connect();
+    // console.log("连接 GATT:", server);
+
+  } catch (err) {
+    console.error("连接失败:", err);
+  }
+}
+
--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ import Player from "./vue/Player";
 import Helper from "./vue/Helper";
 import Algs from "./vue/Algs";
 import { VueConstructor } from "vue/types/umd";
+import { deviceConnect } from "./common/bluetooth"
 
 /* eslint-disable */
 var _hmt: any = _hmt || [];
@@ -59,3 +60,10 @@ const vm = new Vue({
   render: (h): VNode => h(app),
 });
 export default vm;
+
+
+window.addEventListener("dblclick", async () =>{    //   双击事件
+    console.log("请求 22111 设备");
+    deviceConnect();
+
+});
