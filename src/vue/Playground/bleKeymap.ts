5eaf49d90f107e2102c1fefbc53f37f22c6ab70a
10555gff <1073245193@qq.com>
2025-11-08 16:11:02 +0800

    aa

---
+++ src/vue/Playground/bleKeymap.ts
@@ -0,0 +1,14 @@
+export const bleKeymap: { [key: number]: string } = {
+  1:  "L'", 
+  2:  "L", 
+  3:  "R'", 
+  4:  "R", 
+  5:  "D'", 
+  6:  "D", 
+  7:  "U'", 
+  8:  "U", 
+  9:  "F'", 
+  10: "F", 
+  11: "B'", 
+  12: "B",
+};
